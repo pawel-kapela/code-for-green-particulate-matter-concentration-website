c605bee49739e0c35bb325292ff60662eb9a7712
Paweł Kapela <pawel.kapela@protonmail.com>
2020-06-09 09:48:27 +0200

    Small translations adjustment

--- resources/js/components/EmptyRequestResult.js
+++ resources/js/components/EmptyRequestResult.js
@@ -2,13 +2,15 @@ import React from 'react';
 import { Icon } from '@iconify/react';
 import fileEmpty from '@iconify/icons-icomoon-free/file-empty';
 
+import localisations from '../constants/Localisations';
+
 function EmptyRequestResult() {
     const iconSize = '5em';
 
     return (
         <div className="d-flex flex-column align-items-center justify-content-center overflow-auto">
             <Icon icon={fileEmpty} width={iconSize} height={iconSize}/>
-            <h1 className="pt-3 text-center">Brak danych w tym przedziale czasowym</h1>
+            <h1 className="pt-3 text-center">{localisations.emptyRequestResult}</h1>
         </div>
     );
 }
